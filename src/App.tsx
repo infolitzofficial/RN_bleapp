4f72686b8f5a18b324dfd43611aa046dfebe72c2
Muhammed Ashker <ashker@faircodetech.com>
2023-08-07 16:11:03 +0530

    some changes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,18 @@
-import React, { useEffect } from 'react';
-import {
-  View,
-  StyleSheet,
-  useColorScheme,
-} from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import React from 'react';
+import { View, Text } from 'react-native';
 import useApp from './useApp';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const { latitude, longitude } = useApp();
-  console.log(latitude, longitude);
-  // <a href="https://gps-coordinates.org/my-location.php?lat=10.0526937&lng=76.3521401" target="_blank">(10.0526937,76.3521401)</a>
+  const {latitude, longitude} = useApp();
 
   return (
-    <View>
-      <MapView
-        // provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        // ref={ref => map = ref}
-        showsUserLocation={true}
-        showsMyLocationButton={true}
-        region={{
-          latitude: latitude || 10.5276,
-          longitude: longitude || 76.2144,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}>
-        <Marker
-          coordinate={{
-            latitude: latitude ? latitude : 10.5276,
-            longitude: longitude ? longitude : 76.21,
-          }}
-        />
-      </MapView>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <Text>Latitude: {latitude}</Text>
+        <Text>Longitude: {longitude}</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  map: {
-    marginTop: 20,
-    height: 200,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-});
 
 export default App;
--- src/useApp.ts
+++ src/useApp.ts
@@ -1,35 +1,81 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { request, PERMISSIONS } from 'react-native-permissions';
+import { PermissionsAndroid, Platform } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
+import { request, PERMISSIONS } from 'react-native-permissions';
 
 function useApp() {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
 
+  const postData = async () => {
+    try {
+      const res = await axios.post("https://nr5dc5-8000.csb.app/api/tag_new_location", { latitude, longitude });
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    }
+  }
+
   useEffect(() => {
+    perm();
     requestLocationPermission();
+    const interval = setInterval(() => {
+      getCurrentLocation();
+      postData();
+    }, 60000); // 1 minute in milliseconds
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(interval);
   }, []);
 
   const requestLocationPermission = async () => {
-    const permissionStatus = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-    if (permissionStatus === 'granted') {
-      getCurrentLocation();
+    try {
+      const permissionStatus = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      await request(PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION);
+      await Geolocation.requestAuthorization('whenInUse');
+      if (permissionStatus === 'granted') {
+        getCurrentLocation();
+      }
+    } catch (error) {
+      console.error('Error requesting location permission:', error);
     }
   };
 
   const getCurrentLocation = () => {
     Geolocation.getCurrentPosition(
       (position: any) => {
-        const { latitude, longitude } = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
       },
-      (error: any) => {
-        console.log('Error getting location:', error);
+      error => {
+        console.error('Error getting location:', error);
       },
       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
     );
   };
+
+  const perm = async () => {
+    if (Platform.OS === 'ios') {
+      getCurrentLocation();
+    } else {
+      try {
+        const granted = await PermissionsAndroid.requestMultiple(
+          [
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION
+          ],
+        );
+        if (granted[PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION] === PermissionsAndroid.RESULTS.GRANTED) {
+          getCurrentLocation();
+        } else {
+          console.log('Location permission denied');
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    }
+  }
+
   return { latitude, longitude }
 }
 
